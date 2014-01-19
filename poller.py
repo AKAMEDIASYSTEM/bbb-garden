b3a1a385bda5998490abc659b7c9e790a3855187
AKAMEDIASYSTEM <nfeehan@gmail.com>
2014-01-19 14:58:57 -0500

    initial skeleton

--- poller.py
+++ poller.py
@@ -3,28 +3,66 @@
 # -*- coding: utf-8 -*-
 # jan 2014 bbb garden shield attempt
 # AKA
+
+'''
+Sensors:
+analog level sensor, pin AIN0
+TMP102 i2c temperature sensor, address 0x48
+(if add0 is grounded) or 0x49 (if pulled up)
+
+
+Outputs:
+Analog RGB LED strip
+I2C display(?)
+
+'''
 from Adafruit_I2C import Adafruit_I2C
 import time
-import Adafruit_BBIO.UART as UART
-import Adafruit_BBIO.PWM as PWM
-import Adafruit_BBIO.GPIO as GPIO
+import atexit
+import Adafruit_BBIO.UART as uart
+import Adafruit_BBIO.PWM as pwm
+import Adafruit_BBIO.GPIO as gpio
+import Adafruit_BBIO.ADC as adc
 import datetime
 import random
 from tempodb import Client, DataPoint
 import key
 
-# Modify these with your credentials found at: http://tempo-db.com/manage/
+client = Client(key.API_KEY, key.API_SECRET)
+interval = 120 # seconds between samples
+greenPin = 'P8_13'
+bluePin = 'P9_14'
+redPin = 'P8_19'
+servoPin = 'P9_16'
 
-
-client = Client(API_KEY, API_SECRET)
+def exit_handler():
+    print 'exiting'
+    pwm.stop(greenPin)
+    pwm.stop(redPin)
+    pwm.stop(bluePin)
+    pwm.stop(servoPin)
+    pwm.cleanup()
 
 def do_sensor_read():
+	# value = ADC.read("AIN1")
+	# adc returns value from 0 to 1.
+	# use read_raw(pin) to get V values
+
 
 def do_db_update():
 
 def do_state_display():
-	
 
+
+adc.setup()
+# NOTE
+# There is currently a bug in the ADC driver.
+# You'll need to read the values twice
+# in order to get the latest value.
+pwm.start(greenPin, 10.0, 2000.0)
+pwm.start(redPin, 10.0, 2000.0)
+pwm.start(bluePin, 10.0, 2000.0)
+atexit.register(exit_handler)
 
 while True:
 	try:
