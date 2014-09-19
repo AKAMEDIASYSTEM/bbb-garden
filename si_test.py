b2c671b811f1eefd3c07f71e4a34cb741cbd904f
AKAMEDIASYSTEM <nfeehan@gmail.com>
2014-09-19 19:43:40 -0400

    si test

---
+++ si_test.py
@@ -0,0 +1,14 @@
+# Si7005.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# mar 2014 python port of Si7005 arduino library
+# AKA
+
+import Si7005 as si
+import time
+
+s = st.Si7005('P9_21')
+while(1):
+	print s.getTemperature()
+	print s.getHumidity()
+	time.sleep(1)
