e5e41db34fe62c5a33ff3f2ae055dfa9476d8ac9
AKAMEDIASYSTEM <nfeehan@gmail.com>
2014-09-19 20:23:21 -0400

    debugging

--- si_test.py
+++ si_test.py
@@ -9,7 +9,11 @@ import time
 
 s = Si7005.si7005(pin='P9_21')
 # s = Si7005.si7005()
-while(s.detectSensor()):
-	print s.getTemperature()
-	print s.getHumidity()
+while(1):
+	if s.detectSensor():
+		print s.getTemperature()
+		print s.getHumidity()
+else:
+	print 'cannot find sensor'
+	print s
 	time.sleep(1)
